4f3af207d0987d7807626ef696a52ad0134ddac7
Mohd. Imran  Zafar <imran.zafar@irissoftware.com>
2019-06-03 17:31:19 +0530

    latest commits

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import DefaultPage from "./pages/DefaultPage";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div className="text-slanted">Hello from recipe</div>;
+  return (
+    <Router>
+      <main>
+        <Navbar />
+        <Switch>
+          {/* <div className="text-slanted">
+            Hello from recipe
+            <Home />
+            <Recipes />
+            <SingleRecipe />
+            <DefaultPage />
+          </div> */}
+          <Route path="/" exact component={Home} />
+          <Route path="/recipes" exact component={Recipes} />
+          <Route path="/recipes/:id" component={SingleRecipe} />
+          <Route component={DefaultPage} />
+        </Switch>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/DefaultPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class DefaultPage extends Component {
+  render() {
+    return <h4>hello from error page</h4>;
+  }
+}
