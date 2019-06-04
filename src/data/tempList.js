bab37bfb9b46de5a2bc3d865a32bde07854edb98
Mohd. Imran  Zafar <imran.zafar@irissoftware.com>
2019-06-04 17:51:21 +0530

    the recipes page structured

---
+++ src/data/tempList.js
@@ -0,0 +1,47 @@
+export const recipeData = [
+  {
+    publisher: "Closet Cooking",
+    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    source_url:
+      "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+    recipe_id: "35382",
+    image_url:
+      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
+  },
+  {
+    publisher: "Closet Cooking",
+    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    source_url:
+      "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+    recipe_id: "35382",
+    image_url:
+      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
+  },
+  {
+    publisher: "Closet Cooking",
+    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    source_url:
+      "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+    recipe_id: "35382",
+    image_url:
+      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
+  },
+  {
+    publisher: "Closet Cooking",
+    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    source_url:
+      "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+    recipe_id: "35382",
+    image_url:
+      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
+  },
+  {
+    publisher: "Closet Cooking",
+    title: "Jalapeno Popper Grilled Cheese Sandwich",
+    source_url:
+      "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
+    recipe_id: "35382",
+    image_url:
+      "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
+  }
+];
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 import React, { Component } from "react";
 import Header from "../components/Header/Header";
+import { Link } from "react-router-dom";
 export default class Home extends Component {
   render() {
-    return <Header />;
+    return (
+      <Header title="amazing recipes">
+        <Link
+          to="recipes"
+          className="text-uppercase btn btn-secondary btn-large mt-3"
+        >
+          search recipes
+        </Link>
+      </Header>
+    );
   }
 }
