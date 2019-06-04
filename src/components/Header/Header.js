88a86d14c3b1bb47710e5092a353bd536164a4a7
Mohd. Imran  Zafar <imran.zafar@irissoftware.com>
2019-06-04 13:16:23 +0530

    header for foo2work

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,14 @@ import React from "react";
 
 export default function Header() {
   return (
-    <Header>
+    <header>
       <div className="container-fluid">
         <div className="row align-items-center" />
         <div className="col text-center">
-          <h1 className="text-light text-uppercase display-3 letter-spacing text-slanted">
-            {title}
-          </h1>
+          <h1 className="text-light text-uppercase display-3 letter-spacing text-slanted" />
           <p>Hello from Header</p>
         </div>
       </div>
-    </Header>
+    </header>
   );
 }
