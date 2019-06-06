3f9af2281b468d450e10dddb229e3ea79eca66f3
Mohd. Imran  Zafar <imran.zafar@irissoftware.com>
2019-06-06 15:41:44 +0530

    application completed

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -14,16 +14,29 @@ export default class Recipes extends Component {
     search: "",
     url: `https://www.food2fork.com/api/search?key=${
       process.env.REACT_APP_API_KEY
-    }`
+    }`,
+    base_url: `https://www.food2fork.com/api/search?key=${
+      process.env.REACT_APP_API_KEY
+    }`,
+    query: "&q=",
+    error: ""
   };
 
   async getRecipes() {
     try {
       const data = await fetch(this.state.url);
       const jsonData = await data.json();
-      this.setState({
-        recipes: jsonData.recipes
-      });
+      if (jsonData.recipes.length === 0) {
+        this.setState({
+          error:
+            "Sorry but your search didnot return any recipes, please try again or press search icon for the most popular recipes"
+        });
+      } else {
+        this.setState({
+          recipes: jsonData.recipes,
+          error: ""
+        });
+      }
     } catch (e) {
       console.log(e);
     }
@@ -41,6 +54,16 @@ export default class Recipes extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
+    const { base_url, query, search } = this.state;
+    this.setState(
+      {
+        url: `${base_url}${query}${search}`,
+        search: ""
+      },
+      () => {
+        this.getRecipes();
+      }
+    );
   };
 
   render() {
@@ -51,7 +74,19 @@ export default class Recipes extends Component {
           handleChange={this.handleChange}
           handleSubmit={this.handleSubmit}
         />
-        <RecipeList recipes={this.state.recipes} />
+        {this.state.error ? (
+          <section>
+            <div className="row">
+              <div className="col-10 mx-auto my-5">
+                <h2 className="text-orange text-center text-uppercase mt-5">
+                  {this.state.error}
+                </h2>
+              </div>
+            </div>
+          </section>
+        ) : (
+          <RecipeList recipes={this.state.recipes} />
+        )}
       </>
     );
   }
