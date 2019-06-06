3a2a66546ca5d91c1290acd9e704d3d37467dfac
Mohd. Imran  Zafar <imran.zafar@irissoftware.com>
2019-06-06 13:23:21 +0530

    Single recipe page completed

--- src/data/tempList.js
+++ src/data/tempList.js
@@ -13,7 +13,7 @@ export const recipeData = [
     title: "Jalapeno Popper Grilled Cheese Sandwich",
     source_url:
       "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
-    recipe_id: "35382",
+    recipe_id: "35383",
     image_url:
       "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
   },
@@ -22,7 +22,7 @@ export const recipeData = [
     title: "Jalapeno Popper Grilled Cheese Sandwich",
     source_url:
       "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
-    recipe_id: "35382",
+    recipe_id: "35384",
     image_url:
       "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
   },
@@ -31,7 +31,7 @@ export const recipeData = [
     title: "Jalapeno Popper Grilled Cheese Sandwich",
     source_url:
       "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
-    recipe_id: "35382",
+    recipe_id: "35385",
     image_url:
       "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
   },
@@ -40,7 +40,7 @@ export const recipeData = [
     title: "Jalapeno Popper Grilled Cheese Sandwich",
     source_url:
       "http://www.closetcooking.com/2011/04/jalapeno-popper-grilled-cheese-sandwich.html",
-    recipe_id: "35382",
+    recipe_id: "35386",
     image_url:
       "http://static.food2fork.com/Jalapeno2BPopper2BGrilled2BCheese2BSandwich2B12B500fd186186.jpg"
   }
